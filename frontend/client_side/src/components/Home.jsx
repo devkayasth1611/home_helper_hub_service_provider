da3446a769dd6582460130e4bc20cf8888727f99
Dev Kayasth <devkayasth1611@gmail.com>
2024-08-24 20:02:12 -0700

    logo

--- frontend/client_side/src/components/Home.jsx
+++ frontend/client_side/src/components/Home.jsx
@@ -4,23 +4,30 @@ import "../App.css";
 
 function Home() {
   return (
+    <div>
       <div className="banner_section">
-         <div className="container">
-            <div id="main_slider" className="carousel slide" data-ride="carousel">
-               <div className="carousel-inner">
-                  <div className="carousel-item active">
-                     <div className="container">
-                        <div className="row">
-                           <div className="col-md-12">
-                              <h1 className="banner_taital">Electricity Service </h1>
-                              <p className="banner_text">Anything embarrassing hidden in the middle of text. All the Lorem Ipsuanything embarrassing h</p>
-                              <div className="read_bt"><Link to="/Appointment">Book Now</Link></div>
-                           </div>
-                        </div>
-                     </div>
-               </div>
+        <div className="container">
+          <div id="main_slider" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <h1 className="banner_taital">Electricity Service </h1>
+                      <p className="banner_text">
+                        Anything embarrassing hidden in the middle of text. All
+                        the Lorem Ipsuanything embarrassing h
+                      </p>
+                      <div className="read_bt">
+                        <Link to="/Appointment">Book Now</Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-         </div>
+          </div>
+        </div>
       </div>
     </div>
   );
