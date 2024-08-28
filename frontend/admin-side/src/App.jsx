18567dfe3866c43db15e6ac7e131c4be2cca2743
Krishna-Joshi09 <krishnabj94@gmail.com>
2024-08-28 16:23:59 +0530

    admin

--- frontend/admin-side/src/App.jsx
+++ frontend/admin-side/src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
-import Header from './components/header';
-import Sidebar from './components/Sidebar';
+import Header from './components/Header'
+import Home from './components/Home'
+import SiderBar from './components/SiderBar';
+import Footer from './components/Footer';
 
 function App() {
+  
 
   return (
     <>
-     <Router>
-      <Header />
-      <Sidebar />
-      <Routes>
-      </Routes>
-     </Router>
+      <Router>
+        <Header/>
+        <SiderBar/>
+        <Routes>
+    <Route path="/" element={<Home />} />
+
+        </Routes>
+        <Footer/>
+      </Router>
     </>
   )
 }
